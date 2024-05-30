e5b58bb32d4e80cd28a32a4d944afe2f1ab22849
iamrob <rob.duits@incentro.com>
2024-05-30 14:43:01 +0200

    Update widget preview

--- src/NumberIncrement.editorPreview.jsx
+++ src/NumberIncrement.editorPreview.jsx
@@ -1,7 +1,16 @@
 import { createElement } from "react";
 
-export function preview({ inputAttr }) {
-    return <div className="form-control number-increment">[{inputAttr}]</div>;
+export function preview({ inputAttr, inputType }) {
+    return (
+        <div className="number-widget">
+            <div className={`form-control number-increment ${inputType && "has-type"}`}>[{inputAttr}]</div>
+            <div className="number-widget-buttons">
+                <button className="increment">+</button>
+                <button className="decrement">-</button>
+            </div>
+            {inputType && <div className="number-widget-type">{inputType}</div>}
+        </div>
+    );
 }
 
 export function getPreviewCss() {
